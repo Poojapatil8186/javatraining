f115653e43c830959adc5b6de462a80c32d00be4
Poojapatil8186 <poojapatil8186@gmail.com>
2022-05-10 10:36:15 +0530

    initial commit
    Day 11

---
+++ April 19/u nsaved.java
@@ -0,0 +1,7 @@
+/*Write a program that takes as input the size of the array and the elements in the array.
+The program then asks the user to enter a particular index and prints the element at that index. 
+This program may generate Array Index Out Of Bounds Exception. Use exception handling mechanisms to handle this exception. 
+In the catch block, print the class name of the exception thrown. 
+Sample Input and Output 1: Enter the number of elements in the array 3 Enter the elements in the array 20 90 4 Enter the index of the array element you want to access 2 The array element at index 2 = 4 The array element successfully accessed
+Sample Input and Output 2: Enter the number of elements in the array 3 Enter the elements in the array 20 90 4 Enter the index of the array element you want to access 6 java.lang.ArrayIndexOutOfBoundsException
+Exception Handling: Try-catch*/
