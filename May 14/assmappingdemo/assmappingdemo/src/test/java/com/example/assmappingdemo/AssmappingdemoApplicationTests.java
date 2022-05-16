9a87f57387f27718d81f2bbb6b2cdbf2f424115e
Poojapatil8186 <poojapatil8186@gmail.com>
2022-05-16 17:18:27 +0530

    initial commit
    Day 33

---
+++ May 14/assmappingdemo/assmappingdemo/src/test/java/com/example/assmappingdemo/AssmappingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assmappingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssmappingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
