d693f4bb46880e237dda062bba41608d457e57dd
Poojapatil8186 <105098906+Poojapatil8186@users.noreply.github.com>
2022-05-18 10:53:33 +0530

    Delete May 16 directory

--- May 16/onetomanydemo/onetomanydemo/src/main/java/com/example/onetomanydemo/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.onetomanydemo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Items, Integer>{
-
-}
--- May 16/onetomanydemo/onetomanydemo/src/main/java/com/example/onetomanydemo/cartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.onetomanydemo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface cartRepository extends JpaRepository<cart, Integer> {
-
-}
